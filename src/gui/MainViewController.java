a69b06aa80a42a7934da7c398ab8d2de6a6c8b60
raphaelcostar <raphaelcosta.1@hotmail.com>
2022-09-27 16:24:54 -0300

    DepartmentForm (dialog) design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -55,9 +55,9 @@ public class MainViewController implements Initializable {
 	}
 	
 	
-	private synchronized <T> void loadView(String absolutName, Consumer<T> initializingAction) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
 			
 			Scene mainScene = Main.getMainScene();
