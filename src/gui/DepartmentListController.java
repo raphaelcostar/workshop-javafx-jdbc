7b02a6a79d163dda8e0368bc3e8b6d8956d1db0d
raphaelcostar <raphaelcosta.1@hotmail.com>
2022-09-29 16:55:47 -0300

    Saving a new department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -88,6 +88,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDeparmentService(new DepartmentService());
 			controller.updateFormData();
 			
 			
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -12,4 +12,13 @@ public class Utils {
 		
 		
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+	}
 }
